20f7ff123e6042e6b1b1f3d0e0099948f5a38071
ivan_khripunov <khripunov.iv@phystech.edu>
2026-05-17 14:08:40 +0300

    изменил расчет ode

--- src/stage_ode.py
+++ src/stage_ode.py
@@ -1,203 +1,239 @@
 import numpy as np
-from jax import numpy as jnp
+import casadi as ca
 import openmdao.api as om
 from src.frame_converter import EARTH_RAD, EARTH_OMEGA
 
-EARTH_MU = 3.986004418e14  # м^3/с^2, гравитационный параметр Земли
-G0 = 9.80665  # м/с^2, стандартное ускорение
+EARTH_MU = 3.986004418e14  # м³/с²
+G0 = 9.80665  # м/с²
+SGK = 1.7415e-4  # коэффициент Sutton-Graves
 
 
-class StageODE(om.JaxExplicitComponent):
+def _cross(a, b):
+    return ca.vertcat(
+        a[1] * b[2] - a[2] * b[1],
+        a[2] * b[0] - a[0] * b[2],
+        a[0] * b[1] - a[1] * b[0],
+    )
+
+
+def _dot(a, b):
+    return a[0] * b[0] + a[1] * b[1] + a[2] * b[2]
+
+
+def _smooth_h(x, a=0.1):
+    return 0.5 * (1.0 + ca.tanh(x / (2.0 * a)))
+
+
+def rv2oe(rv, vv, mu, a_eps=0.1):
+    eps = 1e-12
+
+    K = ca.vertcat(0.0, 0.0, 1.0)
+    hv = _cross(rv, vv)  # угловой момент
+    nv = _cross(K, hv)  # вектор узла N = ẑ × h
+
+    h2 = ca.fmax(_dot(hv, hv), eps)
+    v2 = ca.fmax(_dot(vv, vv), eps)
+    r = ca.sqrt(ca.fmax(_dot(rv, rv), eps))
+    n = ca.sqrt(ca.fmax(_dot(nv, nv), eps))
+
+    rv_dot_vv = _dot(rv, vv)
+
+    ev = (1.0 / mu) * ((v2 - mu / r) * rv - rv_dot_vv * vv)
+
+    p = h2 / mu
+    e = ca.sqrt(ca.fmax(_dot(ev, ev), eps))
+    a = p / ca.fmax(1.0 - e * e, eps)
+    i_oe = ca.acos(ca.fmax(ca.fmin(hv[2] / ca.sqrt(h2), 1.0 - eps), -1.0 + eps))
+
+    cos_Om = ca.fmax(ca.fmin(nv[0] / n, 1.0 - eps), -1.0 + eps)
+    Om_raw = ca.acos(cos_Om)
+    Om = (_smooth_h(nv[1], a_eps) * Om_raw
+          + _smooth_h(-nv[1], a_eps) * (2.0 * ca.pi - Om_raw))
+
+    nv_dot_ev = _dot(nv, ev)
+    cos_om = ca.fmax(ca.fmin(nv_dot_ev / (n * e), 1.0 - eps), -1.0 + eps)
+    om_raw = ca.acos(cos_om)
+    om = (_smooth_h(ev[2], a_eps) * om_raw
+          + _smooth_h(-ev[2], a_eps) * (2.0 * ca.pi - om_raw))
+
+    ev_dot_rv = _dot(ev, rv)
+    cos_nu = ca.fmax(ca.fmin(ev_dot_rv / (e * r), 1.0 - eps), -1.0 + eps)
+    nu_raw = ca.acos(cos_nu)
+    nu = (_smooth_h(rv_dot_vv, a_eps) * nu_raw
+          + _smooth_h(-rv_dot_vv, a_eps) * (2.0 * ca.pi - nu_raw))
+
+    return ca.vertcat(a, e, i_oe, Om, om, nu)
+
+
+class StageODE(om.ExplicitComponent):
+
     def initialize(self):
         self.options.declare('num_nodes', types=int)
-
         self.options.declare('CD', types=float)
         self.options.declare('S', types=float)
-
         self.options.declare('nose_radius', types=float, default=0.5)
-
         self.options.declare('use_atmosphere', types=bool)
-        self.options.declare('rho_ref', types=float)
-        self.options.declare('h_scale', types=float)
+        self.options.declare('rho_ref', types=float, default=1.225)
+        self.options.declare('h_scale', types=float, default=8.44e3)
+
+        self._fn_map = None
+        self._in_names = []
+        self._out_names = []
+        self._n_in = 0
+        self._n_out = 0
+
+    def _build_casadi(self):
+        nn = self.options['num_nodes']
+        CDA = self.options['CD'] * self.options['S']
+        atm = self.options['use_atmosphere']
+        rho_ref = self.options['rho_ref']
+        h_sc = self.options['h_scale']
+        Rn = self.options['nose_radius']
+
+        self._in_names = ['rx', 'ry', 'rz', 'vx', 'vy', 'vz', 'm',
+                          'dir_x', 'dir_y', 'dir_z', 'throttle',
+                          'thrust', 'Isp', 'm_dry', 'm_propellant']
+        self._n_in = len(self._in_names)
+
+        x = ca.MX.sym('x', self._n_in)
+
+        rx, ry, rz = x[0], x[1], x[2]
+        vx, vy, vz = x[3], x[4], x[5]
+        m = x[6]
+        dx, dy, dz = x[7], x[8], x[9]
+        thr = x[10]
+        thrust = x[11]
+        Isp = x[12]
+
+        # ---- физика ----
+        F_T = thrust * thr
+        dn = ca.sqrt(dx ** 2 + dy ** 2 + dz ** 2 + 1e-12)
+        ex_ = dx / dn
+        ey_ = dy / dn
+        ez_ = dz / dn
+
+        r = ca.sqrt(rx ** 2 + ry ** 2 + rz ** 2)
+        h_alt = r - EARTH_RAD
+
+        inv_r3 = r ** (-3)
+        a_gx = -EARTH_MU * rx * inv_r3
+        a_gy = -EARTH_MU * ry * inv_r3
+        a_gz = -EARTH_MU * rz * inv_r3
+
+        if atm:
+            rho = rho_ref * ca.exp(-h_alt / h_sc)
+            vrx = vx + EARTH_OMEGA * ry
+            vry = vy - EARTH_OMEGA * rx
+            v_rel = ca.sqrt(vrx ** 2 + vry ** 2 + vz ** 2 + 1.0)
+            adx = -0.5 * CDA * rho * v_rel * vrx / m
+            ady = -0.5 * CDA * rho * v_rel * vry / m
+            adz = -0.5 * CDA * rho * v_rel * vz / m
+            q_heat = SGK * ca.sqrt(rho / Rn) * v_rel ** 3
+            q_dyn = 0.5 * rho * v_rel ** 2
+        else:
+            adx = ady = adz = ca.MX(0.0)
+            q_heat = q_dyn = ca.MX(0.0)
+
+        asx = F_T / m * ex_ + adx
+        asy = F_T / m * ey_ + ady
+        asz = F_T / m * ez_ + adz
+        g_load = ca.sqrt(asx ** 2 + asy ** 2 + asz ** 2) / G0
+
+        rxdot = vx
+        rydot = vy
+        rzdot = vz
+        vxdot = a_gx + asx
+        vydot = a_gy + asy
+        vzdot = a_gz + asz
+        mdot = -F_T / (Isp * G0)
+
+        v_mag = ca.sqrt(vx ** 2 + vy ** 2 + vz ** 2)
+        v_radial = (rx * vx + ry * vy + rz * vz) / r
+        dir_nsq = dx ** 2 + dy ** 2 + dz ** 2
+
+        oe = rv2oe(ca.vertcat(rx, ry, rz), ca.vertcat(vx, vy, vz), EARTH_MU)
+
+        out_vec = ca.vertcat(
+            rxdot, rydot, rzdot,
+            vxdot, vydot, vzdot, mdot,
+            r, v_mag, v_radial, dir_nsq, h_alt, F_T,
+            q_heat, q_dyn, g_load,
+            oe[0], oe[1], oe[2], oe[3], oe[4], oe[5],
+        )
+        self._out_names = [
+            'rxdot', 'rydot', 'rzdot',
+            'vxdot', 'vydot', 'vzdot', 'mdot',
+            'r_mag', 'v_mag', 'v_radial', 'dir_norm_sq', 'h', 'thrust_actual',
+            'q_heat', 'q_dyn', 'g_load',
+            'orbit_a', 'orbit_e', 'orbit_inc', 'orbit_raan', 'orbit_arg_periapsis', 'orbit_nu',
+        ]
+        self._n_out = len(self._out_names)
+
+        J_sym = ca.jacobian(out_vec, x)  # (n_out, n_in) — символьно
+
+        fn = ca.Function('ode_and_jac', [x], [out_vec, J_sym])
+
+        self._fn_map = fn.map(nn, 'serial')
 
     def setup(self):
         nn = self.options['num_nodes']
+        self._build_casadi()
 
-        # ---- inputs: состояния ----
         for n in ('rx', 'ry', 'rz'):
             self.add_input(n, val=EARTH_RAD * np.ones(nn), units='m')
         for n in ('vx', 'vy', 'vz'):
             self.add_input(n, val=np.zeros(nn), units='m/s')
-        self.add_input('m', val=1.0e5 * np.ones(nn), units='kg')
-
-        # ---- inputs: управления ----
+        self.add_input('m', val=1e5 * np.ones(nn), units='kg')
         self.add_input('dir_x', val=np.ones(nn))
         self.add_input('dir_y', val=np.zeros(nn))
         self.add_input('dir_z', val=np.zeros(nn))
         self.add_input('throttle', val=np.ones(nn))
-
-        # ---- inputs: параметры ступени ----
         self.add_input('thrust', val=2.1e6 * np.ones(nn), units='N')
         self.add_input('Isp', val=265.2 * np.ones(nn), units='s')
-        self.add_input('m_dry', val=1.0e3 * np.ones(nn), units='kg')
-        self.add_input('m_propellant', val=1.0e5 * np.ones(nn), units='kg')
+        self.add_input('m_dry', val=1e3 * np.ones(nn), units='kg')
+        self.add_input('m_propellant', val=1e5 * np.ones(nn), units='kg')
 
-        # ---- outputs: производные состояний ----
         for n in ('rxdot', 'rydot', 'rzdot'):
             self.add_output(n, val=np.zeros(nn), units='m/s')
         for n in ('vxdot', 'vydot', 'vzdot'):
             self.add_output(n, val=np.zeros(nn), units='m/s**2')
         self.add_output('mdot', val=np.zeros(nn), units='kg/s')
-
-        # ---- outputs: диагностика ----
         self.add_output('r_mag', val=np.zeros(nn), units='m')
         self.add_output('v_mag', val=np.zeros(nn), units='m/s')
         self.add_output('v_radial', val=np.zeros(nn), units='m/s')
         self.add_output('dir_norm_sq', val=np.ones(nn))
         self.add_output('h', val=np.zeros(nn), units='m')
         self.add_output('thrust_actual', val=np.zeros(nn), units='N')
-
         self.add_output('q_heat', val=np.zeros(nn), units='W/m**2')
         self.add_output('q_dyn', val=np.zeros(nn), units='Pa')
-
         self.add_output('g_load', val=np.zeros(nn))
-
         self.add_output('orbit_a', val=EARTH_RAD * np.ones(nn), units='m')
         self.add_output('orbit_e', val=np.zeros(nn))
         self.add_output('orbit_inc', val=np.zeros(nn), units='rad')
-
         self.add_output('orbit_raan', val=np.zeros(nn), units='rad')
         self.add_output('orbit_arg_periapsis', val=np.zeros(nn), units='rad')
+        self.add_output('orbit_nu', val=np.zeros(nn), units='rad')
 
-    def compute_primal(self,
-                       rx, ry, rz,
-                       vx, vy, vz,
-                       m,
-                       dir_x, dir_y, dir_z,
-                       throttle,
-                       thrust, Isp,
-                       m_dry, m_propellant):
+    def setup_partials(self):
+        nn = self.options['num_nodes']
+        ar = np.arange(nn, dtype=int)
+        for o in self._out_names:
+            for i in self._in_names:
+                self.declare_partials(o, i, rows=ar, cols=ar)
 
-        CDA = self.options['CD'] * self.options['S']
-        use_atmosphere = self.options['use_atmosphere']
-        rho_ref = self.options['rho_ref']
-        h_scale = self.options['h_scale']
+    def compute(self, inputs, outputs):
+        X = np.stack([np.asarray(inputs[n]).ravel()
+                      for n in self._in_names], axis=0)
+        outs, _ = self._fn_map(X)
+        out_arr = outs.full()
+        for k, name in enumerate(self._out_names):
+            outputs[name] = out_arr[k, :]
 
-        F_T = thrust * throttle
-
-        # ---- нормировка вектора направления ----
-        dir_norm = jnp.sqrt(dir_x * dir_x + dir_y * dir_y + dir_z * dir_z + 1e-12)
-        dx = dir_x / dir_norm
-        dy = dir_y / dir_norm
-        dz = dir_z / dir_norm
-
-        # ---- геоцентрическое расстояние и высота ----
-        r = jnp.sqrt(rx * rx + ry * ry + rz * rz)
-        h = r - EARTH_RAD
-
-        # ---- центральная гравитация ----
-        inv_r3 = 1.0 / (r * r * r)
-        a_grav_x = -EARTH_MU * rx * inv_r3
-        a_grav_y = -EARTH_MU * ry * inv_r3
-        a_grav_z = -EARTH_MU * rz * inv_r3
-
-        # ---- атмосферное сопротивление (опционально) ----
-        if use_atmosphere:
-            rho = rho_ref * jnp.exp(-h / h_scale)
-
-            v_atm_x = -EARTH_OMEGA * ry
-            v_atm_y = EARTH_OMEGA * rx
-            vrx = vx - v_atm_x
-            vry = vy - v_atm_y
-            vrz = vz
-            v_rel = jnp.sqrt(vrx * vrx + vry * vry + vrz * vrz + 1.0)
-
-            a_drag_x = -0.5 * CDA * rho * v_rel * vrx / m
-            a_drag_y = -0.5 * CDA * rho * v_rel * vry / m
-            a_drag_z = -0.5 * CDA * rho * v_rel * vrz / m
-
-            SUTTON_GRAVES_K = 1.7415e-4
-            nose_radius = self.options['nose_radius']
-            q_heat = SUTTON_GRAVES_K * jnp.sqrt(rho / nose_radius) * v_rel ** 3
-            q_dyn = 0.5 * rho * v_rel * v_rel
-        else:
-            a_drag_x = jnp.zeros_like(vx)
-            a_drag_y = jnp.zeros_like(vy)
-            a_drag_z = jnp.zeros_like(vz)
-            q_heat = jnp.zeros_like(vx)
-            q_dyn = jnp.zeros_like(vx)
-
-        # ---- ускорение от тяги ----
-        a_thrust_x = (F_T / m) * dx
-        a_thrust_y = (F_T / m) * dy
-        a_thrust_z = (F_T / m) * dz
-
-        # ---- ускорение от внешних сил ----
-        a_spec_x = a_thrust_x + a_drag_x
-        a_spec_y = a_thrust_y + a_drag_y
-        a_spec_z = a_thrust_z + a_drag_z
-        g_load = jnp.sqrt(a_spec_x ** 2 + a_spec_y ** 2 + a_spec_z ** 2) / G0
-
-        # ---- производные состояний ----
-        rxdot = vx
-        rydot = vy
-        rzdot = vz
-        vxdot = a_grav_x + a_spec_x
-        vydot = a_grav_y + a_spec_y
-        vzdot = a_grav_z + a_spec_z
-        mdot = -F_T / (Isp * G0)
-
-        # ---- диагностика ----
-        r_mag = r
-        v_mag = jnp.sqrt(vx * vx + vy * vy + vz * vz)
-        v_radial = (rx * vx + ry * vy + rz * vz) / r
-        dir_norm_sq = dir_x * dir_x + dir_y * dir_y + dir_z * dir_z
-        thrust_actual = F_T
-
-        # ---- Орбитальные элементы ----
-        v2_orb = vx * vx + vy * vy + vz * vz
-        rdotv_orb = rx * vx + ry * vy + rz * vz
-
-        eps_orb = 0.5 * v2_orb - EARTH_MU / r
-        orbit_a = -EARTH_MU / (2.0 * eps_orb)
-
-        hx = ry * vz - rz * vy
-        hy = rz * vx - rx * vz
-        hz = rx * vy - ry * vx
-        h_norm = jnp.sqrt(hx * hx + hy * hy + hz * hz + 1e-12)
-
-        coef_r = v2_orb / EARTH_MU - 1.0 / r
-        coef_v = rdotv_orb / EARTH_MU
-        ex = coef_r * rx - coef_v * vx
-        ey = coef_r * ry - coef_v * vy
-        ez = coef_r * rz - coef_v * vz
-        orbit_e = jnp.sqrt(ex * ex + ey * ey + ez * ez + 1e-12)
-
-        cos_i = jnp.clip(hz / h_norm, -1.0 + 1e-12, 1.0 - 1e-12)
-        orbit_inc = jnp.arccos(cos_i)
-
-        Nx = -hy
-        Ny = hx
-        # Nz = 0  по определению
-        n_norm = jnp.sqrt(Nx * Nx + Ny * Ny + 1e-12)
-
-        orbit_raan = jnp.arctan2(Ny, Nx)  # ∈ [-π, π]
-
-        hxN_x = -hz * hx
-        hxN_y = -hz * hy
-        hxN_z = hx * hx + hy * hy  # = n_norm²
-
-        # N · e и (h × N) · e
-        N_dot_e = Nx * ex + Ny * ey
-        hxN_dot_e = hxN_x * ex + hxN_y * ey + hxN_z * ez
-
-        # ω = atan2((h × N) · e, |h| · (N · e))
-        orbit_arg_periapsis = jnp.arctan2(hxN_dot_e, h_norm * N_dot_e)
-
-        return (rxdot, rydot, rzdot,
-                vxdot, vydot, vzdot,
-                mdot,
-                r_mag, v_mag, v_radial, dir_norm_sq, h, thrust_actual,
-                q_heat, q_dyn, g_load,
-                orbit_a, orbit_e, orbit_inc,
-                orbit_raan, orbit_arg_periapsis)
+    def compute_partials(self, inputs, partials):
+        X = np.stack([np.asarray(inputs[n]).ravel()
+                      for n in self._in_names], axis=0)
+        _, J_dm = self._fn_map(X)
+        J = J_dm.full()
+        for oi, oname in enumerate(self._out_names):
+            for ii, iname in enumerate(self._in_names):
+                partials[oname, iname] = J[oi, ii::self._n_in]
